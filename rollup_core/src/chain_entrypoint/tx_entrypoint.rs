33bad4438036532f450ca21bf2dac000936d0887
AhzamAkhtar <ahzam8186@gmail.com>
2025-02-07 19:13:04 +0530

    connection half done

--- rollup_core/src/chain_entrypoint/tx_entrypoint.rs
+++ rollup_core/src/chain_entrypoint/tx_entrypoint.rs
@@ -1,5 +1,8 @@
-use solana_sdk::pubkey::Pubkey;
+use std::{collections::HashMap, default};
+
+use solana_sdk::{blake3::Hash, pubkey::Pubkey};
 use crate::{line_up_queue::line_up_queue::{AccountInvolvedInTransaction, LineUpQueue}, scheduler::read_write_locks::ThreadAwareLocks};
+
 
 pub struct  MakeTransaction {
     id : u64,
@@ -8,17 +11,65 @@ pub struct  MakeTransaction {
     priority_level : u64
 }
 
-impl MakeTransaction {
-    pub fn create_new_transaction(&mut self,id:u64,tx_type : String, account : AccountInvolvedInTransaction , priority : u64) -> Self {
-        Self {
+pub struct ChainTransaction {
+    pub chain_transaction  : HashMap<u64, MakeTransaction>
+}
+
+
+pub struct TransactionsOnThread {
+    pub trnasaction_on_thread  : HashMap<u64,usize>
+}
+
+impl TransactionsOnThread {
+    fn init() -> Self {
+        let trnasaction_on_thread_hashmp = HashMap::new();
+        TransactionsOnThread {
+            trnasaction_on_thread : trnasaction_on_thread_hashmp
+        }
+    }
+
+    // thread_id -> trnasaction_id
+    pub fn get_all_tx_ids_for_thread(&self, thread_id: usize) -> Vec<u64> {
+        let mut result = Vec::new();
+        for (&id, &tid) in self.trnasaction_on_thread.iter() {
+            if tid == thread_id {
+                result.push(id);
+            }
+        }
+        result
+    }
+}
+
+impl Default for ChainTransaction  {
+    fn default() -> Self {
+        let chain_trnasaction = HashMap::new();
+        ChainTransaction {
+            chain_transaction : chain_trnasaction
+        }
+    }
+}
+
+impl ChainTransaction {
+
+    pub fn create_new_transaction(&mut self,id:u64,tx_type : String, account : AccountInvolvedInTransaction , priority : u64) -> MakeTransaction  {
+        self.chain_transaction.insert(id, MakeTransaction {
+            id : id,
+            tx_type : tx_type.clone(),
+            accounts : AccountInvolvedInTransaction {
+                is_writeable_accounts : account.is_writeable_accounts.clone(),
+                non_writeable_accounts : account.non_writeable_accounts.clone()
+            },
+            priority_level : priority
+        });   
+        MakeTransaction {
             id : id,
             tx_type : tx_type,
             accounts : AccountInvolvedInTransaction {
-                is_writeable_accounts : vec![],
-                non_writeable_accounts : vec![]
+                is_writeable_accounts : account.is_writeable_accounts,
+                non_writeable_accounts : account.non_writeable_accounts
             },
-            priority_level : priority
-        }
+            priority_level : priority 
+        }     
     }
 
     pub fn push_new_transaction_to_the_main_queue(&mut self, lineup_queue : &mut LineUpQueue) {
@@ -28,7 +79,7 @@ impl MakeTransaction {
             non_writeable_accounts : vec![]
         },1);
 
-        lineup_queue.add_to_main_tx_queue(
+        lineup_queue.add_to_main_tx_queue( 
             new_transaction.id,
             new_transaction.tx_type,
             new_transaction.accounts,
@@ -52,14 +103,16 @@ impl MakeTransaction {
     // full-up the transaction from lineup_queue and apply RW locks and schedule on threads
     pub fn take_out_individual_transaction_and_apply_RWlocks(lineup_queue : &mut LineUpQueue, thread_aware_locks : &mut ThreadAwareLocks) {
         let transactions: Vec<_> = lineup_queue.lineup_queue.iter().cloned().collect();
+        //TODO:
+        //do not init trnasaction_on_thread as default, use existing value
+        let mut trnasaction_on_thread =  TransactionsOnThread::init();
         for transaction in transactions {
 
             let is_writeable_accounts_clone = transaction.txs_accounts.is_writeable_accounts.clone();
             let non_writeable_accounts_clone = transaction.txs_accounts.non_writeable_accounts.clone();
 
             if let Some(scheduled_thread) = thread_aware_locks.try_lock_account(is_writeable_accounts_clone.clone(), non_writeable_accounts_clone.clone()) {
-                //TODO:
-                //do something with scheduled_thread
+                trnasaction_on_thread.trnasaction_on_thread.insert(transaction.id, scheduled_thread);
             }
             else {
                 lineup_queue.add_transaction_to_non_rescheduable_container(transaction.id, transaction.tx_type, transaction.txs_accounts, transaction.priority);
@@ -67,6 +120,29 @@ impl MakeTransaction {
             
         }
     }
+
+    pub fn get_single_transaction_on_a_particular_thread(&self, id : u64) -> &MakeTransaction {
+        let transaction = self.chain_transaction.get(&id).unwrap();
+        transaction
+    }
+
+    pub fn get_all_transaction_on_a_thread(&mut  self,  tsx_on_thread : TransactionsOnThread, thread_id : usize) -> Vec<&MakeTransaction> {
+        let ids = TransactionsOnThread::get_all_tx_ids_for_thread(&tsx_on_thread, thread_id);
+        let mut all_transaction_on_thread_id = Vec::new();
+        for id in ids { 
+            let single_txs = self.get_single_transaction_on_a_particular_thread(id);
+            all_transaction_on_thread_id.push(single_txs);
+        }
+        all_transaction_on_thread_id
+    }
+
+    pub fn get_all_transaction_from_all_4_thread(&mut self, tsx_on_thread : TransactionsOnThread) {
+        let transaction_on_thread_1  = self.get_all_transaction_on_a_thread(tsx_on_thread, 1);
+        // let transaction_on_thread_2  = self.get_all_transaction_on_a_thread(tsx_on_thread, 2);
+        // let transaction_on_thread_3  = self.get_all_transaction_on_a_thread(tsx_on_thread, 3);
+        // let transaction_on_thread_4  = self.get_all_transaction_on_a_thread(tsx_on_thread, 4);
+    }
+
 
 
 }
