947adab0b307652c10aed197abab325f474b061a
AhzamAkhtar <ahzam8186@gmail.com>
2025-02-25 04:12:24 +0530

    update

--- rollup_core/src/chain_entrypoint/transaction_context.rs
+++ rollup_core/src/chain_entrypoint/transaction_context.rs
@@ -1,7 +1,7 @@
 use actix_web::web::get;
 use solana_sdk::pubkey::Pubkey;
 
-#[derive(Debug,Clone)]
+#[derive(Debug,Clone,Copy)]
 pub struct InstructionAccount {
     pub index_in_transaction : usize,
     pub index_in_caller : usize,
@@ -21,7 +21,7 @@ pub struct InstructionContext {
 
 #[derive(Debug,Clone)]
 pub struct TransactionContext {
-    pub account_keys: Pubkey,
+    //pub account_keys: Pubkey,
     //pub accounts: Rc<TransactionAccounts>,
     // instruction_stack_capacity: usize,
     // instruction_trace_capacity: usize,
@@ -42,33 +42,18 @@ pub struct TransactionContext {
     // signature: Signature,
 }
 
-impl InstructionAccount {
-    pub fn create_native_instruction_account_for_transaction(&mut self , index_in_transaction : usize,is_signer:bool,is_writeable:bool) -> Self {
-        Self {
-            index_in_transaction,
-            index_in_caller : 0,
-            index_in_callee : None,
-            is_signer,
-            is_writeable
-
-        }
-    }
-
-    pub fn create_main_instruction_account_for_transaction(&mut self, index_in_transaction : usize , is_signer:bool , is_writeable:bool , context_stack_height : usize) -> Self {
-        Self {
-            index_in_transaction,
-            index_in_caller : context_stack_height,
-            index_in_callee : Some(0),
-            is_signer,
-            is_writeable
-        }
-    }
-}
-
 impl Default for InstructionContext {
     fn default() -> Self {
         Self { 
             instruction_accounts: Vec::new() 
+        }
+    }
+}
+
+impl Default for TransactionContext {
+    fn default() -> Self {
+        Self {
+            instruction_trace : Vec::new()
         }
     }
 }
@@ -82,31 +67,103 @@ impl InstructionContext {
         return self.instruction_accounts.len();
     }
 
-    pub fn get_native_ins_account(&mut self , index_in_transaction : usize) -> Option<&InstructionAccount> {
-        for native_instruction in &self.instruction_accounts {
-            if native_instruction.index_in_callee == None && native_instruction.index_in_transaction == index_in_transaction {
-                return Some(native_instruction);
-            }
-        }
-        None
+    pub fn create_native_instruction_account_for_transaction(&mut self , index_in_transaction : usize,is_signer:bool,is_writeable:bool) {
+        let instruction_account = InstructionAccount {
+            index_in_transaction,
+            index_in_caller : 0,
+            index_in_callee : None,
+            is_signer,
+            is_writeable
+
+        };
+        self.add_instruction_context(instruction_account);
     }
+
+    pub fn create_main_instruction_account_for_transaction(&mut self, index_in_transaction : usize , is_signer:bool , is_writeable:bool , context_stack_height : usize) {
+        let instruction_account = InstructionAccount {
+            index_in_transaction,
+            index_in_caller : context_stack_height,
+            index_in_callee : Some(0),
+            is_signer,
+            is_writeable
+        };
+        self.add_instruction_context(instruction_account);
+    }
+
+
 }
 
 impl TransactionContext {
-    
-    pub fn check_for_accounts_permission_previlage_mismatch(&mut self,instruction_context : &mut  InstructionContext) {
-        let instruction_context_vec = &mut self.instruction_trace;
-        for (index , instruction_accounts) in instruction_context_vec.iter().enumerate() {
-            if let Some(my_instruction) = instruction_accounts.instruction_accounts.get(index) {
-                let native_instruction = InstructionContext::get_native_ins_account( instruction_context, index);
-                if native_instruction.unwrap().is_writeable != my_instruction.is_writeable {
+
+    pub fn check_for_accounts_permission_previlage_mismatch(&mut self,instruction_context :  InstructionContext,account_index:usize) {
+       // let instruction_context_vec = &mut instruction_context.instruction_accounts;
+        // println!("{:?}",self.instruction_trace.len());
+        println!("{:?}",instruction_context);
+        for (index , instruction_account) in instruction_context.instruction_accounts.iter().enumerate() {
+            println!("{:?} {:?}",index,instruction_account);
+                let native_instruction = get_native_ins_account(instruction_context.clone() ,account_index).unwrap();
+                println!("native_ins{:?}",native_instruction);
+                if native_instruction.is_writeable != instruction_account.is_writeable {
                     panic!("Writeable previlage esclated")
                 }
-                if native_instruction.unwrap().is_signer != my_instruction.is_signer {
+                if native_instruction.is_signer != instruction_account.is_signer {
                     panic!("Signer previlage esclated")
                 }
 
-            }
+            
         } 
     }
+}
+
+
+
+pub fn get_native_ins_account(instruction_context : InstructionContext , index_in_transaction : usize) -> Option<InstructionAccount> {
+    for (index,native_instruction) in instruction_context.instruction_accounts.iter().enumerate() {
+        println!("milaab {:?} {:?} {:?}",index,native_instruction.index_in_callee,native_instruction.index_in_transaction);
+        if native_instruction.index_in_callee == None && native_instruction.index_in_transaction == index_in_transaction {
+            return Some(*native_instruction);
+        }
+    }
+    None
+}
+
+mod test {
+    use crate::chain_entrypoint::transaction_context::TransactionContext;
+
+    use super::{InstructionAccount, InstructionContext};
+
+    #[test]
+    fn test_transaction_context_flow() {
+        let mut instruction_context = InstructionContext::default();
+        let mut transaction_context = TransactionContext::default();
+
+        let from_native_ins = instruction_context.create_native_instruction_account_for_transaction(
+            0,true,true
+        );
+
+        // let to_native_ins = instruction_context.create_native_instruction_account_for_transaction(
+        //     1,true,true
+        // );
+
+        let stack_height = instruction_context.get_context_stack_height();
+
+        let main_from_ins = instruction_context.create_main_instruction_account_for_transaction(0, false, false, stack_height);
+
+        assert_eq!(
+            stack_height,
+            1
+        );
+
+        assert_eq!(
+            instruction_context.instruction_accounts.len(),
+            2
+        );
+
+        // let stack_height = instruction_context.get_context_stack_height();
+
+        // let main_to_ins = instruction_context.create_main_instruction_account_for_transaction(1, true, false, stack_height);
+
+        //println!("{:?}",instruction_context.instruction_accounts);
+        transaction_context.check_for_accounts_permission_previlage_mismatch(instruction_context,0);
+    } 
 }
